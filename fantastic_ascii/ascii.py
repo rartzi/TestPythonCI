2a78111f7bf6c73892279123a3ba0b916c7ebcdb
rartzi <ronen.artzi@gmail.com>
2021-04-09 23:08:23 -0400

    added packages related example

---
+++ fantastic_ascii/ascii.py
@@ -0,0 +1,23 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+# Ronen Artzi
+
+def joe_say(text):
+    template = r'''
+    =-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=
+    // {message}    \\
+     =-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=
+       \\
+        \\
+           ----------------
+          /                \
+         /                  \
+        |     OO      O0     |
+        |     OO      OO     |
+         \         -        /
+          \     DDDDDD     /
+           \     DDDD     /
+            \____________/
+    '''.format(message=text)
+
+    return template
